a5d32151feb27616ab6152f1b8da33d9b8c2f5b6
Mohd Sarik Momin Ansari <sarik17111@gmail.com>
2025-07-08 21:37:39 +0530

    Fixed Lombok issues and successful build

--- src/main/java/com/ansari/model/Cart.java
+++ src/main/java/com/ansari/model/Cart.java
@@ -1,34 +1,26 @@
 package com.ansari.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.util.ArrayList;
-import java.util.List;
-
 
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Cart {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-
     private Long id;
 
-    @OneToOne
+    @ManyToOne
     private User customer;
 
-    private Long total;
+    // other fields...
 
-    @OneToMany(mappedBy = "cart" , cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<CartItem> item = new ArrayList<>();
+    public User getCustomer() {
+        return customer;
+    }
 
+    public void setCustomer(User customer) {
+        this.customer = customer;
+    }
 
-
-
-
+    // other getters/setters
 }
--- src/main/java/com/ansari/model/User.java
+++ src/main/java/com/ansari/model/User.java
@@ -3,18 +3,11 @@ package com.ansari.model;
 import com.ansari.dto.Restaurantdto;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-
 public class User {
 
     @Id
@@ -29,47 +22,79 @@ public class User {
 
     private USER_ROLE role = USER_ROLE.ROLE_CUSTOMER;
 
-//    public USER_ROLE getRole() {
-//        return this.role;
-//    }
-//
-//    public String getEmail() {
-//        return email;
-//    }
-//
-//    public String getPassword() {
-//        return password;
-//    }
-
-
     @JsonIgnore
     @OneToMany(cascade = CascadeType.ALL , mappedBy = "customer")
     private List<Order> orders = new ArrayList<>();
 
     @ElementCollection
-    private List<Restaurantdto> favorites = new ArrayList();
+    private List<Restaurantdto> favorites = new ArrayList<>();
 
     @OneToMany(cascade = CascadeType.ALL , orphanRemoval = true)
-    private List<Address> addresses = new ArrayList();
+    private List<Address> addresses = new ArrayList<>();
 
+    // Getters and setters
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public USER_ROLE getRole() {
+        return role;
+    }
+
+    public void setRole(USER_ROLE role) {
+        this.role = role;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
+
+    public List<Restaurantdto> getFavorites() {
+        return favorites;
+    }
+
+    public void setFavorites(List<Restaurantdto> favorites) {
+        this.favorites = favorites;
+    }
+
+    public List<Address> getAddresses() {
+        return addresses;
+    }
+
+    public void setAddresses(List<Address> addresses) {
+        this.addresses = addresses;
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
